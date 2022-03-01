1d7ccaf2ee75811c0d5cacd0dc92f0942cf7de25
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-03-01 08:33:49 -0300

    FirstProgram

---
+++ Aula10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte X = 12;
+            Console.WriteLine(X);
+        }
+
+
+    }
+}
